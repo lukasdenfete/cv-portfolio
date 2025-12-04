e5eed884e4b2f57de3e4a1593664506a7471a037
lukasdenfete <153369307+lukasdenfete@users.noreply.github.com>
2025-12-04 11:28:58 +0100

    email-funktionalitet för kontaktformuläret.

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,10 +1,31 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import ContactInfo from "./ContactInfo";
 import { COLORS } from "../Colors";
+import emailjs from '@emailjs/browser';
 import AOS from 'aos';
 
 function Contact() {
   const [buttonHover, setButtonHover] = useState(false);
+
+  const form = useRef<HTMLFormElement>(null);
+  const sendEmail = (e: React.FormEvent) => {
+    e.preventDefault();
+  const SERVICE_ID = "service_th0kz2j";
+  const TEMPLATE_ID = "template_zx2i07k";
+  const PUBLIC_KEY = "LtPUGefu5pFaIC8Yz";
+
+  if(form.current) {
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY).then((result) => {
+      console.log(result.text);
+      alert("Ditt meddelande har skickats.");
+      (e.target as HTMLFormElement).reset();
+    }, (error) => {
+      console.log(error.text);
+      alert("Något gick fel. Försök igen senare.");
+    });
+  }
+  };
+
 
   return (
     <div className="max-w-5xl mx-auto px-4 py-12 overflow-hidden">
@@ -20,9 +41,10 @@ function Contact() {
           <h3 className="text-xl font-semibold mb-6 text-gray-700 text-center pb-2">
             Skicka ett meddelande
           </h3>
-          <form className="space-y-4">
+          <form ref={form} onSubmit={sendEmail} className="space-y-4">
             <input
               type="text"
+              name="from_name"
               placeholder="Namn"
               className="w-full p-3 border rounded"
               style={{
@@ -34,6 +56,7 @@ function Contact() {
             />
             <input
               type="email"
+              name="from_email"
               placeholder="E-post"
               className="w-full p-3 border rounded"
               style={{
@@ -44,6 +67,7 @@ function Contact() {
               required
             />
             <textarea
+              name="message"
               placeholder="Meddelande..."
               rows={5}
               className="w-full p-3 border rounded"
