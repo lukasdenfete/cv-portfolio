b58e114560a585fcdc28805ca2d82c37d42a720e
lukasdenfete <153369307+lukasdenfete@users.noreply.github.com>
2025-12-13 22:59:58 +0100

    fixat stavfel

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,7 @@ function About() {
         <p>
         Lukas heter jag och studerar för närvarande sista året på systemvetenskapliga programmet vid Örebro Universitet. 
         Jag har under min utbildning utvecklat ett stort intresse för systemutveckling och cybersäkerhet. 
-        Den är sidan har jag byggt för att träna lite på front-end, och är tänkt att fungera som en portfolio 
+        Den här sidan har jag byggt för att träna lite på front-end, och är tänkt att fungera som en portfolio 
         som visar mitt CV och lite olika projekt jag jobbat med i skolan och på fritiden. Vill du komma i 
         kontakt med mig finns alla uppgifter här på sidan, eller så kan du skicka ett meddelande via 
         formuläret. Om inte annat kan du ju beundra den fina bilden jag tog medan jag åkte luftballong i Mexiko. 
